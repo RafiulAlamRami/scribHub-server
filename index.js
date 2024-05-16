ba83a7ed5c04aa4e8ff39551afd2724f0ef41f31
RafiulAlamRami <rafiul7368@gmail.com>
2024-05-16 15:21:18 +0600

    create getwish api

--- index.js
+++ index.js
@@ -156,6 +156,13 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/getwish/:email',async(req,res)=>{
+      const email=req.params.email
+      const query={email:email}
+      const result=await wishCollection.findOne(query)
+      res.send(result)
+    })
+
     app.get('/blogDetails/:id',async(req,res)=>{
       const id=req.params.id
       const query={_id:new ObjectId(id)}
